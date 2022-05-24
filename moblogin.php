eb7fbb65621db6e5843783a67f2b26765df0d03a
MearegAbate <49908493+MearegAbate@users.noreply.github.com>
2022-05-24 10:09:54 +0300

    Update moblogin.php

--- moblogin.php
+++ moblogin.php
@@ -25,29 +25,33 @@ if(isset($_POST['but_submit'])){
 
 }
 ?>
-<html>
-    <head>
-        <title>Yesrasew payment system</title>
-     <link href="style.css" rel="stylesheet" type="text/css">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
     </head>
     <body>
         <div class="container">
             <form method="post" action="">
                 <div id="div_login">
                     <h1>Login Yesera Sew Payment System</h1>
-                    <div>
-                        <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
-                    </div>
-                    <div>
-                        <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
-                    </div>
-                    <div>
-                        <input type="submit" value="Submit" name="but_submit" id="but_submit" />
+                    
+                        <input type="text" class="form-control" id="txt_uname" name="txt_uname" placeholder="Username" />
+                  
+                        <input type="password" class="form-control" id="txt_uname" name="txt_pwd" placeholder="Password"/>
+                   
+                        <input type="submit" class="btn btn-success" value="Submit" name="but_submit" id="but_submit" />
                         <a href="">Back</a> </br>
-                    </div>
+                    
                 </div>
             </form>
         </div>
-    </body>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+
 </html>
 
