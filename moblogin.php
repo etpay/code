3a92b5776589d88ac54e820d6b44b1e21fc13b1b
MearegAbate <49908493+MearegAbate@users.noreply.github.com>
2022-05-24 10:47:35 +0300

    Update moblogin.php

--- moblogin.php
+++ moblogin.php
@@ -34,21 +34,23 @@ if(isset($_POST['but_submit'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
    </head>
-   <body>
-        <h4> Yesera Sew Payment System</h4>
+   <body></br>
+    <div  class="container-sm"> 
+        <h5 class="text-center"> Yesera Sew Payment System</h5></br>
         <form method="post" action="">
-            <h2>Login</h2>
-        <input type="text" class="form-control"  name="txt_uname" placeholder="Username">
-        <input type="password" class="form-control" name="txt_pwd"  placeholder="Password">
+            <h2 class="text-center"> Login</h2></br>
+        <input type="text" class="form-control"  name="txt_uname" placeholder="Username" required></br>
+        <input type="password" class="form-control" name="txt_pwd"  placeholder="Password" required></br>
       <!-- Submit button -->
       <div class="d-grid gap-2">
-      <button type="submit" class="btn btn-primary ">Sign in</button>
+      <button type="submit" class="btn btn-primary ">Sign in</button></br>
             </div>
       <!-- Register buttons -->
       <div class="text-center">
         <p>Not a member? <a href="mobregister.php">Register</a></p>
       </div>
-    </form>     
+    </form>    
+</div>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
 </html>
