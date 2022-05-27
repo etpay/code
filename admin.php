655185cc30951264151791f084d9ef215f49cc17
gossaye <51283865+gossaye@users.noreply.github.com>
2022-05-27 11:20:57 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,125 +1,109 @@
-<!Doctype html>
-<html>
+
+<?php
+include 'auth_session.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+   </head>
 <body>
-<style>
-#nav {
-list-style:none inside;
-margin:0;
-padding:0;
-text-align:center;
-}
-#nav li {
-display:block;
-position:relative;
-float:left;
-background: #24af15; /* menu background color */
-}
-#nav li a {
-display:block;
-padding:0;
-text-decoration:none;
-width:200px; /* this is the width of the menu items */
-line-height:35px; /* this is the hieght of the menu items */
-color:#ffffff; /* list item font color */
-}
-#nav li li a {font-size:80%;} /* smaller font size for sub menu items */
-#nav li:hover {background:#003f20;} /* highlights current hovered list item and the parent list items when hovering over sub menues */
-#nav ul {
-position:absolute;
-padding:0;
-left:0;
-display:none; /* hides sublists */
-}
-#nav li:hover ul ul {display:none;} /* hides sub-sublists */
-#nav li:hover ul {display:block;} /* shows sublist on hover */
-#nav li li:hover ul {
-display:block; /* shows sub-sublist on hover */
-margin-left:200px; /* this should be the same width as the parent list item */
-margin-top:-35px; /* aligns top of sub menu with top of list item */
-}
-</style>
-</head>
-<ul id="nav">
-<li><a href="#">Yeserasew Payment System</a></li>
-<li><a href="#">Telecom</a>
-<ul>
 
 
-<li><a href="#">»</a>
-<ul>
-<li><a href="index.php">Ethio telecom</a>
-<li><a href="index.php">Safaricom</a>
-</ul>
-</li>
-</ul>
-</li>
+	<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="mobcoment.php">Comment</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Account</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mobpartner.php">Partner</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="mobpayment.php">Payment</a>
+        </li>
+	      <li class="nav-item">
+          <a class="nav-link disabled" href="moblogout.php">Log out</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div class="table-responsive">
 
-</ul>
-<ul id="nav">
-
-<li><a href="#">Power</a>
-<ul>
-
-
-<li><a href="#">»</a>
-<ul>
-<li><a href="index.php">Electric</a>
-<li><a href="index.php">Electric</a>
-</ul>
-</li>
-</ul>
-</li>
-
-</ul>
-<ul id="nav">
-
-<li><a href="#">Water and sanitation</a>
-<ul>
-
-
-<li><a href="#">»</a>
-<ul>
-<li><a href="index.php">Water and sanitation</a>
-<li><a href="index.php">sanitation</a>
-</ul>
-</li>
-</ul>
-</li>
-
-</ul>
-<ul id="nav">
-
-<li><a href="#">Housing and land Related</a>
-<ul>
-
-
-<li><a href="#">»</a>
-<ul>
-<li><a href="index.php">A.A Land House</a>
-<li><a href="index.php">others</a>
-</ul>
-</li>
-</ul>
-</li>
-
-</ul>
-<ul id="nav">
-
-<li><a href="#">Tax and pension Related</a>
-<ul>
+<?php
+ 
+	require 'db.php';
+	try {
+	$query = "SELECT * FROM commit";
+	$stmt = $dbc->prepare($query);
+	$stmt->execute();
+	echo "<table class='table table-dark  table-hover'>";
+	echo "<tr>";
+	echo "<th>cu_id</th>";
+	echo "<th>name</th>";
+	echo "<th>partner</th>";
+	echo "<th>electric</th>";
+	
+	echo "<th>Day left</th>";
+	echo "<th>telecom</th>";
+	echo "<th>Day left</th>";
+	echo "<th>water</th>";
+	echo "<th>Day left</th>";
+	echo "<th>total</th>";
+	
+	echo "<th>New Month Payment</th>";
+	echo "</tr>";
+	while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+		extract($row);
+		 $elec = new DateTime($edeadline);
+		 $tel = new DateTime($tdeadline);
+		 $wat = new DateTime($wdeadline);
+        
+        $now = new DateTime();
+         
+        // $difference = $now->diff($dob);
+        $eday = $now->diff($elec)->format("%r%a");
+        $tday = $now->diff($tel)->format("%r%a");
+        $wday = $now->diff($wat)->format("%r%a");
+         
+        // $day = $difference->d;
+	echo "<tr>";
+	echo "<td>{$cu_id}</td>";
+	echo "<td>{$name}</td>";
+	echo "<td>{$partner}</td>";
+	echo "<td>{$electric}</td>";
+	echo "<td>{$eday}</td>";
+	echo "<td>{$telecom}</td>";
+	echo "<td>{$tday}</td>";
+	echo "<td>{$water}</td>";
+	echo "<td>{$wday}</td>";
+	$total = ($row['electric'] + $row['telecom'] + $row['water']) ;
+	echo "<td>{$total}</td>";
+	echo "<td><a href='edit.php?cu_id={$cu_id}'>New Month</a> </td>";
+	
 
 
-<li><a href="#">»</a>
-<ul>
-<li><a href="index.php">A.A  Revenue Authority</a>
-<li><a href="index.php">FDRE Ministry of Revenue</a>
-</ul>
-</li>
-</ul>
-</li>
-
-</ul>
-<a href="login.php">Log Out</a> </br>
-
-</body>
+	echo "</tr>";
+	}
+	} catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+	
+?>
+	</table>
+	</div>
+	
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
 </html>
