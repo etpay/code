efe8d78439a5407130da4e39d859608ce8919b16
gossaye <51283865+gossaye@users.noreply.github.com>
2022-05-27 14:49:14 +0300

    Update searchbyid.php

--- searchbyid.php
+++ searchbyid.php
@@ -5,8 +5,49 @@ include 'auth_session.php';
 <html>
 <head>
 	<title>Search you info</title>
+	<style>
+.note
+{
+    text-align: center;
+    height: 80px;
+    background: -webkit-linear-gradient(left, #0072ff, #8811c5);
+    color: #fff;
+    font-weight: bold;
+    line-height: 80px;
+}
+.form-content
+{
+    padding: 5%;
+    border: 1px solid #ced4da;
+    margin-bottom: 2%;
+}
+.form-control{
+    border-radius:1.5rem;
+}
+.btnSubmit
+{
+    border:none;
+    border-radius:1.5rem;
+    padding: 1%;
+    width: 20%;
+    cursor: pointer;
+    background: #0062cc;
+    color: #fff;
+}
+
 </head>
 <body>
+		<div class="container register-form">
+			<div class="container register-form">
+            <div class="form">
+                <div class="note">
+                    <p>Yesrasew payment system</p>
+                </div>
+
+                <div class="form-content">
+                    <div class="row">
+                        <div class="col-md-6">
+
     <form method="post">
         <label>Search By Id</label>
         <input type="text" name="search">
