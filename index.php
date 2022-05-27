eac7330794c8c81a90ec293598c88750708c8926
gossaye <51283865+gossaye@users.noreply.github.com>
2022-05-27 11:09:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,50 @@ include 'auth_session.php';
 <!DOCTYPE html>
 <html>
 	<head>
+<style>
+.note
+{
+    text-align: center;
+    height: 80px;
+    background: -webkit-linear-gradient(left, #0072ff, #8811c5);
+    color: #fff;
+    font-weight: bold;
+    line-height: 80px;
+}
+.form-content
+{
+    padding: 5%;
+    border: 1px solid #ced4da;
+    margin-bottom: 2%;
+}
+.form-control{
+    border-radius:1.5rem;
+}
+.btnSubmit
+{
+    border:none;
+    border-radius:1.5rem;
+    padding: 1%;
+    width: 20%;
+    cursor: pointer;
+    background: #0062cc;
+    color: #fff;
+}
 
+</style>
 	</head>
 		<body>
+			<div class="container register-form">
+			<div class="container register-form">
+            <div class="form">
+                <div class="note">
+                    <p>Yesrasew payment system</p>
+                </div>
+
+                <div class="form-content">
+                    <div class="row">
+                        <div class="col-md-6">
+
 				<?php
 				if(isset($_GET['cu_id'])) {
 					require 'config.php';
