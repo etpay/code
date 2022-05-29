8d67ab15845a09e3456261560657c9716b78066e
MearegAbate <mearegabate@gmail.com>
2022-05-29 11:06:57 +0300

    q

--- mob_index.php
+++ mob_index.php
@@ -15,8 +15,8 @@ include 'mob_auth_session.php';
 <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <span class="navbar-toggler-icon">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        Menu
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
