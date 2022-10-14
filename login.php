82af945d93d0575f02f9eb9200ff791b3f4d6d72
MearegAbate <49908493+MearegAbate@users.noreply.github.com>
2022-10-14 13:28:36 +0300

    Update login.php

--- login.php
+++ login.php
@@ -24,73 +24,13 @@ body {
 }
 
 </style><?php
-include "db/config.php";
-    session_start([
-    'cookie_httponly' => true,
-    'cookie_secure' => true
-]);
 
-if(isset($_POST['but_submit'])){
-// 
-// (B) CHECK IF TOKEN IS SET
-if (!isset($_POST["token"]) || !isset($_SESSION["token"]) || !isset($_SESSION["token-expire"])) {
-  exit("Token is not set!");
-}
- 
-// (C) COUNTER CHECK SUBMITTED TOKEN AGAINST SESSION
-if ($_SESSION["token"]==$_POST["token"]) {
-  // (C1) EXPIRED
-  if (time() >= $_SESSION["token-expire"]) {
-    exit("Token expired. Please reload form.");
-  }
-  // (C2) OK - DO YOUR PROCESSING
-  else {
-//     unset($_SESSION["token"]);
-//     unset($_SESSION["token-expire"]);
-    echo "OK";
-  }
-}
-
-// (D) INVALID TOKEN
-else { exit("INVALID TOKEN*-"._SESSION["token"].'-------------'.$_POST["token"]); }
 // 
 // 
-    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
-    $pass = mysqli_real_escape_string($con,$_POST['txt_pwd']);
-    $password = md5($pass);
 
-
-    if ($uname != "" && $password != ""){
-
-        $sql_query = "select role from users where username='".$uname."' and password='".$password."'";
-        $result = mysqli_query($con,$sql_query);
-        $row = mysqli_fetch_array($result);
-
-       $count = ($row==null) ? 0 : count($row) ;
-
-        if($count > 0){
-            
-            if ($row['role']=='Admin') {
-                 $_SESSION['uname'] = $uname;
-                header('Location:register.php');
-            } else {
-                $_SESSION['uname'] = $uname;
-                header('Location:admin.php');
-            }
-            
-        }else{
-           echo "<script>alert('Not meet with credentials.')</script>";
-        }
-
-    }
-
-}
-// 
-// 
-else{
 $_SESSION["token"] = bin2hex(random_bytes(32));
 $_SESSION["token-expire"] = time() + 1800; // 1 hour = 3600 secs
-}
+
 // 
 // 
 ?>
@@ -101,7 +41,7 @@ $_SESSION["token-expire"] = time() + 1800; // 1 hour = 3600 secs
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
-                        <form  autocomplete="off"  id="login-form" class="form" action="" method="post">
+                        <form  autocomplete="off"  id="login-form" class="form" action="login-cal.php" method="post">
                             <!--  -->
             <input type="hidden" name="token" value="<?=$_SESSION["token"]?>"/>
             <!--  -->
